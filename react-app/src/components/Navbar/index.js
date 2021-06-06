a6219075b57da26295eee6cca39e110137f5e117
Brandon Forster <brandonforster@live.com>
2021-06-05 23:30:09 -0600

    authentification bug fix

--- react-app/src/components/Navbar/index.js
+++ react-app/src/components/Navbar/index.js
@@ -80,7 +80,7 @@ const NavBar = ({ authenticated, setAuthenticated, loaded, setLoaded }) => {
                   Account
                 </p>
                 <LogoutButton
-                  authenticated={authenticated}
+                  authenticated={setAuthenticated}
                   setAuthenticated={setAuthenticated}
                 />
               </ul>
